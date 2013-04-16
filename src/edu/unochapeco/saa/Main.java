0469caa960bf44b9b825c55bdd162e00325d2f10
Andrei J. Zuse <andrei@gamatec.com.br>
2013-04-16 17:57:56 -0300

    Adicionado servidor REST ao projeto

--- src/edu/unochapeco/saa/Main.java
+++ src/edu/unochapeco/saa/Main.java
@@ -1,29 +1,61 @@
 package edu.unochapeco.saa;
 
+import IceBreakRestServer.IceBreakRestServer;
 import java.io.IOException;
 
 /**
  *
  * @author Daniel Girotto
+ * IceBreak Rest Server
+ * https://code.google.com/p/icebreakrest/wiki/IceBreak
  */
 public class Main {
 
     public static void main(String[] args) {
-        Login login = new Login();
-        
-        try {
-            if(login.connect("", ""))
-            {
-                String session = login.getSession();
-                MaterialApoio apoio = new MaterialApoio(session);
-                String materiais = apoio.getMateriais();
+        IceBreakRestServer rest;   // Declare the HTTP server class
 
-                System.out.println(materiais);
+        try { 
+            rest  = new IceBreakRestServer();  
+            rest.setPort(90);
+
+            while (true) {
+                rest.getHttpRequest();     
+                String usuario = rest.getQuery("usuario");
+                String senha = rest.getQuery("senha");
+                String info = rest.getQuery("info");
+
+
+                if(usuario != null || senha != null)
+                {
+                    Login login = new Login();
+                    login.connect(usuario, senha);
+                    String sessao = login.getSession();
+
+                    NotasGraduacao notas = new NotasGraduacao(sessao);
+                    MaterialApoio material = new MaterialApoio(sessao);
+                    HorariosSemestre horarios = new HorariosSemestre(sessao);
+
+                    if(info == null || info.equalsIgnoreCase("notas")){
+                       rest.write(notas.getNotas() + "\n");
+                    }
+
+                    if (info == null || info.equalsIgnoreCase("materiais"))
+                    {
+                        rest.write(material.getMateriais() + "\n");
+                    }
+
+                    if(info == null || info.equalsIgnoreCase("horarios"))
+                    {
+                        rest.write(horarios.getHorarios() + "\n");
+                    }
+                }
+                else {
+                    rest.write("Parâmetros Inválidos");
+                }
             }
-            else
-                System.out.println("Login Invalido");
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
